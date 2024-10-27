dd8b19b46125364bdec4a9b7dd1588c16d62198a
keke125 <admin@keke125.com>
2024-10-27 15:37:48 +0800

    chore: Update MUI core and DataGrid locale to zh-TW.

--- theme.js
+++ theme.js
@@ -1,12 +1,18 @@
 'use client';
 
 import { createTheme } from '@mui/material/styles';
+import { zhTW } from '@mui/x-data-grid/locales';
+import { zhTW as coreZhTW } from '@mui/material/locale';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: 'var(--font-noto-sans-tc)',
+const theme = createTheme(
+  {
+    typography: {
+      fontFamily: 'var(--font-noto-sans-tc)',
+    },
+    cssVariables: true,
   },
-  cssVariables: true,
-});
+  zhTW,
+  coreZhTW
+);
 
 export default theme;
