2109f3e32d19aa6bdd9e891454692b6d8982ba91
keke125 <admin@keke125.com>
2024-12-16 22:10:32 +0800

    chore: Add Dockerfile for building Docker image.

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  /*
+  // For development usage
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:8080/api/:path*',
+        }
+      ]
+    },
+    */
+  output: "standalone",
+};
