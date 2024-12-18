02d31c6f53645001844eaa3513d85978a9d37b92
keke125 <admin@keke125.com>
2024-12-18 22:00:54 +0800

    feat: Add Progressive Web App (PWA) support.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,25 @@
-module.exports = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   /*
   // For development usage
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:8080/api/:path*',
-        }
-      ]
-    },
-    */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      }
+    ]
+  },
+  */
   output: "standalone",
-};
+}
+
+module.exports = withPWA(nextConfig);
