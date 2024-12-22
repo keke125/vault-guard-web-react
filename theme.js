95e74d016156c0d062a425126266ec65a0e56fa9
keke125 <admin@keke125.com>
2024-12-22 15:32:38 +0800

    fix: Add missing translations.

--- theme.js
+++ theme.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { createTheme } from '@mui/material/styles';
-import { zhTW } from '@mui/x-data-grid/locales';
+import { zhTW } from './app/locales/zhTW';
 import { zhTW as coreZhTW } from '@mui/material/locale';
 
 const theme = createTheme(
