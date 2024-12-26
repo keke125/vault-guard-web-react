1fab49c78ebf6fab95db3ac1f42660d123a62e30
keke125 <admin@keke125.com>
2024-12-26 23:33:19 +0800

    feat: Implement partial account page with fetch functionality to activate account.

---
+++ app/activate-account/[email]/page.js
@@ -0,0 +1,232 @@
+"use client"
+
+import { Typography, Box, FormControl, FormLabel } from '@mui/material';
+import { TextField, Button } from '@mui/material';
+import { Alert, Snackbar } from '@mui/material';
+import * as React from 'react';
+import { redirect, usePathname, useSearchParams } from 'next/navigation';
+import { Card } from '../../components/Card';
+import { ActivateAccountContainer } from '../../components/ActivateAccountContainer';
+
+export default function ActivateAccountByEmail() {
+
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    const [email, setEmail] = React.useState('');
+    const [emailError, setEmailError] = React.useState(false);
+    const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+    const [token, setToken] = React.useState('');
+    const [tokenError, setTokenError] = React.useState(false);
+    const [tokenErrorMessage, setTokenErrorMessage] = React.useState('');
+    const [alert, setAlert] = React.useState(false);
+    const [alertMessage, setAlertMessage] = React.useState('');
+
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
+    const validateInputs = () => {
+        const email = document.getElementById('email');
+        const token = document.getElementById('token');
+
+        let isValid = true;
+
+        if (!email.value || !validateEmail(email.value)) {
+            setEmailError(true);
+            setEmailErrorMessage('請輸入有效的電子信箱!');
+            isValid = false;
+        } else {
+            setEmailError(false);
+            setEmailErrorMessage('');
+        }
+
+        if (!token.value) {
+            setTokenError(true);
+            setTokenErrorMessage('請輸入驗證碼!');
+            isValid = false;
+        } else {
+            setTokenError(false);
+            setTokenErrorMessage('');
+        }
+
+        return isValid;
+    };
+
+    const submitData = async (email) => {
+        let activateAccountStatus = false;
+        await fetch('/api/v1/account/activate-account', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: email,
+                token: token
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else if (response.status === 400) {
+                    return response.json().then((response) => { throw new Error(`啟用失敗，${response["message"]}`) });
+                } else {
+                    throw new Error("啟用失敗，請檢查電子信箱是否正確!");
+                }
+            }).then(
+                (response) => {
+                    setAlert(true);
+                    setAlertMessage(response["message"]);
+                    activateAccountStatus = true;
+                }
+            ).catch(
+                (error) => {
+                    if (error.message) {
+                        setAlert(true);
+                        setAlertMessage(error.message);
+                    }
+                }
+            );
+        if (activateAccountStatus) {
+            redirect("/log-in", "push");
+        }
+    }
+
+    const handleSubmit = (event) => {
+        if (emailError | tokenError) {
+            event.preventDefault();
+            return;
+        }
+        const data = new FormData(event.currentTarget);
+        submitData(data.get('email'));
+        event.preventDefault();
+    };
+
+    React.useEffect(() => {
+        if (pathname) {
+            if (validateEmail(pathname.replace("/activate-account/", ""))) {
+                setEmail(pathname.replace("/activate-account/", ""));
+            }
+        }
+        if (searchParams.get('token')) {
+            setToken(searchParams.get('token'));
+        }
+    }, [pathname, searchParams]);
+
+    return (
+        <ActivateAccountContainer direction="column" justifyContent="space-between">
+            <Card variant="outlined">
+                <Typography
+                    component="h1"
+                    variant="h4"
+                    sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+                >
+                    啟用帳號
+                </Typography>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    noValidate
+                    method="post"
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '100%',
+                        gap: 2,
+                    }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor="email">電子信箱</FormLabel>
+                        <TextField
+                            error={emailError}
+                            helperText={emailErrorMessage}
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={email}
+                            placeholder="請輸入電子信箱"
+                            autoComplete="email"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={emailError ? 'error' : 'primary'}
+                            onChange={(event) => {
+                                setEmail(event.target.value);
+                            }}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor="token">驗證碼</FormLabel>
+                        <TextField
+                            error={tokenError}
+                            helperText={tokenErrorMessage}
+                            id="token"
+                            type="token"
+                            name="token"
+                            value={token}
+                            placeholder="請輸入確認信裡的驗證碼，或直接點擊信中連結"
+                            autoComplete="token"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={tokenError ? 'error' : 'primary'}
+                            onChange={(event) => {
+                                setToken(event.target.value);
+                            }}
+                        />
+                    </FormControl>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        onClick={validateInputs}
+                    >
+                        啟用帳號
+                    </Button>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        onClick={() => redirect("/activate-account", "push")}
+                    >
+                        重新取得確認信
+                    </Button>
+                    <Button
+                        fullWidth
+                        onClick={() => redirect("/log-in", "push")}
+                    >
+                        登入
+                    </Button>
+                </Box>
+            </Card>
+            {alert ?
+                <Snackbar
+                    open={alert}
+                    autoHideDuration={6000}
+                    onClose={() => { setAlert(false); setAlertMessage(''); }}
+                >{
+                        alertMessage.includes("!") ? <Alert
+                            onClose={() => { setAlert(false); setAlertMessage(''); }}
+                            severity="warning"
+                            variant="filled"
+                            sx={{ width: '100%' }}
+                        >
+                            {alertMessage}
+                        </Alert> : <Alert
+                            onClose={() => { setAlert(false); setAlertMessage(''); }}
+                            severity="success"
+                            variant="filled"
+                            sx={{ width: '100%' }}
+                        >
+                            {alertMessage}
+                        </Alert>
+                    }
+                </Snackbar> : <></>}
+        </ActivateAccountContainer>
+    )
+}
---
+++ app/activate-account/page.js
@@ -0,0 +1,177 @@
+"use client"
+
+import { Typography, Box, FormControl, FormLabel } from '@mui/material';
+import { TextField, Button } from '@mui/material';
+import { Alert, Snackbar } from '@mui/material';
+import * as React from 'react';
+import { redirect } from 'next/navigation';
+import { Card } from '../components/Card';
+import { ActivateAccountContainer } from '../components/ActivateAccountContainer';
+
+export default function ActivateAccount() {
+
+    const [emailError, setEmailError] = React.useState(false);
+    const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+    const [alert, setAlert] = React.useState(false);
+    const [alertMessage, setAlertMessage] = React.useState('');
+
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
+    const validateInputs = () => {
+        const email = document.getElementById('email');
+
+        let isValid = true;
+
+        if (!email.value || !validateEmail(email.value)) {
+            setEmailError(true);
+            setEmailErrorMessage('請輸入有效的電子信箱!');
+            isValid = false;
+        } else {
+            setEmailError(false);
+            setEmailErrorMessage('');
+        }
+
+        return isValid;
+    };
+
+    const submitData = async (email) => {
+        let activateAccountStatus = false;
+        await fetch('/api/v1/account/activate-account', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: email
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else if (response.status === 400) {
+                    return response.json().then((response) => { throw new Error(`啟用失敗，${response["message"]}`) });
+                } else {
+                    throw new Error("啟用失敗，請檢查電子信箱是否正確!");
+                }
+            }).then(
+                (response) => {
+                    setAlert(true);
+                    setAlertMessage(response["message"]);
+                    activateAccountStatus = true;
+                }
+            ).catch(
+                (error) => {
+                    if (error.message) {
+                        setAlert(true);
+                        setAlertMessage(error.message);
+                    }
+                }
+            );
+        if (activateAccountStatus) {
+            redirect(`/activate-account/${email}`, "push");
+        }
+    }
+
+    const handleSubmit = (event) => {
+        if (emailError) {
+            event.preventDefault();
+            return;
+        }
+        const data = new FormData(event.currentTarget);
+        submitData(data.get('email'));
+        event.preventDefault();
+    };
+
+    return (
+        <ActivateAccountContainer direction="column" justifyContent="space-between">
+            <Card variant="outlined">
+                <Typography
+                    component="h1"
+                    variant="h4"
+                    sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+                >
+                    啟用帳號
+                </Typography>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    noValidate
+                    method="post"
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '100%',
+                        gap: 2,
+                    }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor="email">電子信箱</FormLabel>
+                        <TextField
+                            error={emailError}
+                            helperText={emailErrorMessage}
+                            id="email"
+                            type="email"
+                            name="email"
+                            placeholder="請輸入電子信箱"
+                            autoComplete="email"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={emailError ? 'error' : 'primary'}
+                        />
+                    </FormControl>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        onClick={validateInputs}
+                    >
+                        寄送啟用帳號確認信
+                    </Button>
+                    <Button
+                        fullWidth
+                        onClick={() => redirect("/log-in", "push")}
+                    >
+                        登入
+                    </Button>
+                    <Button
+                        fullWidth
+                        onClick={() => redirect("/sign-up", "push")}
+                    >
+                        註冊
+                    </Button>
+                </Box>
+            </Card>
+            {alert ?
+                <Snackbar
+                    open={alert}
+                    autoHideDuration={6000}
+                    onClose={() => { setAlert(false); setAlertMessage(''); }}
+                >{
+                        alertMessage.includes("!") ? <Alert
+                            onClose={() => { setAlert(false); setAlertMessage(''); }}
+                            severity="warning"
+                            variant="filled"
+                            sx={{ width: '100%' }}
+                        >
+                            {alertMessage}
+                        </Alert> : <Alert
+                            onClose={() => { setAlert(false); setAlertMessage(''); }}
+                            severity="success"
+                            variant="filled"
+                            sx={{ width: '100%' }}
+                        >
+                            {alertMessage}
+                        </Alert>
+                    }
+                </Snackbar> : <></>}
+        </ActivateAccountContainer>
+    )
+}
